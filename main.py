fad0a10a6c25510ff905b8a86a69eb5eb17d2ab8
Gustavo Amaral <gugamaral@gmail.com>
2024-05-28 21:02:25 -0300

    Versão 1.1

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
-dados = []
+import os
 
-while True:
-    dados.sort()
-    novo_dado = input('Informe os dados na lista a seguir: ')
-
-    if novo_dado != '':
-        dados.append(novo_dado)
-        continue
-    else:
-        break
 pessoa = {
-    
-    'nome':'',
-    'cpf':'',
-    'rg':'',
-    'data de Nascimento':'',
-    'gênero':'',
-    'e-mail':'',
-    'telefone':'',
-    'tipo sanguíneo':'',
-    'profissão':'',
-    'empresa':''
-    }
 
-pessoa['nome'] = input('Informe o nome: ')
-pessoa['idade'] = int(input('Informe a idade: '))
-pessoa['profissao'] = input('Informe a profissão: ')
+    'Nome': input('Informe seu nome: '),
+    'CPF': input('Informe seu CPF: '),
+    'RGg': input('Informe seu RG: '),
+    'Data de Nascimento': input('Informe sua data de nascimento: '),
+    'Gênero': input('Informe seu gênero: '),
+    'e-mail': input('Informe seu e-mail: '),
+    'Telefone': input('Informe seu telefone: '),
+    'Tipo Aanguíneo': input('Informe seu tipo sanguíneo: '),
+    'Profissão': input('Informe sua profissão: '),
+    'Empresa': input('Informe sua empresa: ')
+
+}
+os.system('cls')
+print(f'{'-'*30} DADOS DA PESSOA {'-'* 30} \n ')
+
 
 for chave in pessoa:
     print(f'{chave}: {pessoa.get(chave)}')
